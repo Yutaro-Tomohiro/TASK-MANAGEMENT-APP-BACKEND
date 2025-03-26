68284f9bb23ec7e296fc93fadab60e6818f7934c
Yutaro-Tomohiro <tomohiro1751@gmail.com>
2025-03-27 03:38:47 +0900

    tasksテーブルとuser_tasksテーブルを追加

---
+++ db/migrate/20250326154521_create_tasks.rb
@@ -0,0 +1,14 @@
+class CreateTasks < ActiveRecord::Migration[8.0]
+  def change
+    create_table :tasks do |t|
+      t.string :title, null: false
+      t.text :text
+      t.integer :priority, default: 0, null: false
+      t.integer :status, default: 0, null: false
+      t.datetime :begins_at
+      t.datetime :ends_at
+
+      t.timestamps
+    end
+  end
+end
