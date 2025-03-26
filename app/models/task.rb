957a63a663efaead3483a64e72d3dbaac19af58e
Yutaro-Tomohiro <tomohiro1751@gmail.com>
2025-03-27 03:39:27 +0900

    モデルの追加と修正

---
+++ app/models/task.rb
@@ -0,0 +1,11 @@
+class Task < ApplicationRecord
+  has_many :user_tasks, dependent: :nullify
+  has_many :users, through: :user_tasks
+
+  enum :priority, { low: 0, medium: 1, high: 2 }, prefix: true
+  enum :status, { not_started: 0, in_progress: 1, completed: 2 }, prefix: true
+
+  validates :title, presence: true
+  validates :priority, presence: true
+  validates :status, presence: true
+end
