5a8715c37efaf2319658ded52909ef7c805ef760
Yutaro-Tomohiro <tomohiro1751@gmail.com>
2025-03-26 12:22:17 +0900

    UserRepositoryのテストを修正

--- spec/factories/credentials.rb
+++ spec/factories/credentials.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: credentials
+#
+#  id         :integer          not null, primary key
+#  email      :string           not null
+#  password   :string           not null
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryBot.define do
   factory :credential do
     email { "test_user@example.com" }
