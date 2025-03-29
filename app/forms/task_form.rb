d1c180d454283b27803298b430dc3a681fc40cd3
Yutaro-Tomohiro <tomohiro1751@gmail.com>
2025-03-29 17:02:33 +0900

    タスク詳細を取得するときにパスパラメータのidがuuidかをバリデーションするように修正

--- app/forms/task_form.rb
+++ app/forms/task_form.rb
@@ -4,4 +4,5 @@ class TaskForm
   attr_accessor :identity
 
   validates :identity, presence: true
+  validates :identity, format: { with: /\A[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\z/i }
 end
